5dde054ac6e52a1688986a97c2ac6addb56b2dc1
Chapsone <126812939+Chapsone@users.noreply.github.com>
2023-05-15 17:13:45 +0200

    Add files via upload

--- Controller.py
+++ Controller.py
@@ -5,30 +5,41 @@ import time
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import LaserScan
 import numpy as np
-import matplotlib.pyplot as plt
 import pygame
+
+import threading
+
 
 class controller(Node):
 
     def __init__(self):
-        super().__init__("mannette")
+        # Parameters
+        self.msg = Twist()
+        self.msg.linear.x = 0.0
+        self.msg.angular.z = 0.0
+
         pygame.joystick.init()
-        joysticks = [pygame.joystick.Joystick(x) for x in range (pygame.joystick.get_count())]
+        joysticks = [pygame.joystick.Joystick(x)
+                      for x in range
+                        (pygame.joystick.get_count())]
         pygame.init()
 
-       
-
+        super().__init__("mannette")
         self.cmd_pub_= self.create_publisher(Twist, "/cmd_vel",10)
-        self.create_timer(0.5, self.callback(joysticks))
+        # self.create_timer(0.5, self.callback)
 
-    def callback(self, joysticks):
-        msg=Twist()
-        print(joysticks)
-        print("Karim")
-        vx=0.0
-        vz=0.0
+        # creat Thread
+        self.thread = threading.Thread(target=self.main_class(joysticks))
+        self.thread.start()
+
+    def callback(self):
+        print(str(self.msg.linear.x) + " " + str(self.msg.angular.z))	
+        self.cmd_pub_.publish(self.msg)
+
+    def main_class(self, joysticks):
+        vx = 0.0
+        vz = 0.0
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.JOYHATMOTION:
@@ -54,13 +65,12 @@ class controller(Node):
                     if pygame.joystick.Joystick(0).get_button(2):
                         vz=0.0
                         
-                msg.linear.x= vx
-                msg.angular.z= vz
-                print(str(vx) + " " + str(vz))	
-                self.cmd_pub_.publish(msg)
+                self.msg.linear.x = vx
+                self.msg.angular.z = vz
+                print(str(self.msg.linear.x) + " "
+                       + str(self.msg.angular.z))	
+                self.cmd_pub_.publish(self.msg)
                 time.sleep(0.01)
-
-
 
 def main(args=None):
     rclpy.init(args=args)
